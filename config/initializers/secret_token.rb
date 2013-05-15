e165bbe44b5730ecbea98dc8436dcfe4a9a9b52a
rebelcolony <kevin@rebelcolony.com>
2013-05-15 18:43:04 +0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EmployeeTracker::Application.config.secret_token = 'b6619fa0d8600d3e4edda79dc872bd02a9a961bb0c6227b0746fec2d1b542e92b01b22d745bf88c73eab4b9c46ab6022ae3b8f63ae6440c7fb8dd62edb724f82'
